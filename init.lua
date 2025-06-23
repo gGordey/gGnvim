285c4c06ea4e4bd8438977abddd4e06047be8bce
gGordey <ggordeymini@gmail.com>
2025-06-23 23:04:08 +0300

    mapping

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 vim.opt.shiftwidth = 4
 vim.opt.number = true
+
 vim.opt.relativenumber = true
-
 require ("core.mappings")
-
--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>u", function() vim.cmd("m +1") end, opt)
+vim.keymap.set("n", "<leader>d", function() vim.cmd("m -2") end, opt)
+vim.keymap.set("n", "<leader>rd", "<c-r>")
+
 vim.keymap.set("n", "<leader>lp", vim.cmd.Ex)
+
